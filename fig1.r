3670ea99846ca658ede175abc37cab274d52eb21
Wei-Cheng Yang <rainyday309@gmail.com>
2016-07-26 21:03:32 +0800

    completed plot of entropy versus scale

--- fig1.r
+++ fig1.r
@@ -8,25 +8,35 @@ library('ggplot2')
 library('grid')
 library('gridExtra')
 
+
+# transform summarized data for plotting
 p<-completer[,27:786]
-completer_transformed <- as.data.frame(colMeans(p))
+completer_transformed <- as.data.frame(colMeans(p), stringsAsFactors = FALSE)
 completer_transformed['name'] <- rownames(completer_transformed)
-completer_transformed['se'] <- as.data.frame(apply(X = p,MARGIN = 2, FUN = sd))/length(completer[,1])
+completer_transformed['se'] <- as.data.frame(apply(X = p,MARGIN = 2, FUN = sd))/sqrt(length(completer[,1]))
 colnames(completer_transformed)[1]='means'
 
 rm(p)
 
 something <-regmatches(completer_transformed$name,regexec(pattern = "(pre|post)_([a-zA-Z1-9]{2,3})_(.*)",text = completer_transformed$name))
 something <-unlist(something)
-something <-as.data.frame(t(matrix(something,nrow=4,ncol=760)))
+something <-as.data.frame(t(matrix(something,nrow=4,ncol=760)),stringsAsFactors = FALSE)
 
 transformed <- data.frame(name=completer_transformed$name, means=completer_transformed$means, ses=completer_transformed$se,prepost=something$V2,location=something$V3,measure=something$V4 ,stringsAsFactors = FALSE)
 rm(something)
 rm(completer_transformed)
 
-df <- data.frame(x=c(1:10),  
-                 y=completer_means[1:10,'pre_F7'],
-                 ymin=completer_means[1:10,'pre_F7']-completer_ses[1:10,'pre_F7'],
-                 ymax=completer_means[1:10,'pre_F7']+completer_ses[1:10,'pre_F7'])
+# completed transform
+
+# select only scales
+transformed_scales <- filter(transformed, grepl(x=measure, pattern='scale\\d'))
+transformed_scales['scale'] <-as.numeric(t(matrix(unlist(regmatches(transformed_scales$measure, regexec(pattern = "scale([0-9]+)",text=transformed_scales$measure))),nrow=2,ncol=380))[,2])
+
+
+topic <- filter(transformed_scales,location=='F7')
+
+topic$ymin <- topic$means-topic$ses
+topic$ymax <- topic$means+topic$ses
+
 pd <- position_dodge(0.1)
-plot <- ggplot(df,aes(x=x,y=y)) + geom_point() + geom_errorbar(aes(ymin=ymin, ymax=ymax)) + ggtitle('pre_F7')
+plot <- ggplot(topic,aes(x=scale,y=means,color=prepost)) + geom_line() + geom_errorbar(aes(ymin=ymin, ymax=ymax)) + ggtitle('entropy change over scales at F7')
